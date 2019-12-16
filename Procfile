7341c522acb4bcfc4ff0f3554234a542ec92ec76
Jeff Tian <jie.tian@unicareer.com>
2019-12-16 21:09:07 +0800

    fix: --worker-class sanic.worker.GunicornWorker

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --timeout 600 --bind 0.0.0.0:$PORT wsgi
+web: gunicorn --timeout 600 --bind 0.0.0.0:$PORT wsgi --worker-class sanic.worker.GunicornWorker
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import os
 from src.web import app as application
 
-# if __name__ == '__main__':
-    # application.run(host='0.0.0.0', port=int(os.environ['PORT']), debug=False)
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=int(os.environ['PORT']), debug=False)
