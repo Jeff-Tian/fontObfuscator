d156cb748695d98960eda1db9572a23959b32166
Jeff Tian <jie.tian@unicareer.com>
2019-12-16 20:49:16 +0800

    debug

--- src/web.py
+++ src/web.py
@@ -89,4 +89,4 @@ async def encrypt(request):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ['PORT']), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ['PORT']), debug=True)
